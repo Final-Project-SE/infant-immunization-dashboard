56c09d9ce855499eca4e7a960322059301d39b39
natnael772 <natnaeldeyas0@gmail.com>
2024-03-20 14:34:53 +0300

    feat: Add dialog for updating admin

---
+++ src/components/modal/update-admin-modal.tsx
@@ -0,0 +1,107 @@
+import { ReactElement, useState } from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+function UpdateAdminModal({
+  admin,
+  onUpdate,
+  children,
+}: {
+  admin: any;
+  onUpdate: Function;
+  children: ReactElement;
+}) {
+  const { firstName, lastName, email } = admin;
+
+  const [open, setOpen] = useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const handleSubmit = () => {
+    onUpdate();
+    handleClose();
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
+        {children}
+        {/* <Button variant="outline">Add New User</Button> */}
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[650px] ">
+        <DialogHeader>
+          <DialogTitle>Update admin</DialogTitle>
+          <DialogDescription>
+            Make changes to admin's information here. Click update when you're
+            done.
+          </DialogDescription>
+        </DialogHeader>
+
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="firstName" className="text-left">
+              First name
+            </Label>
+            <Input
+              id="firstName"
+              defaultValue={firstName}
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="lastName" className="text-left">
+              Last name
+            </Label>
+            <Input
+              id="last_name"
+              defaultValue={lastName}
+              className="col-span-3"
+            />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="email" className="text-left">
+              Email
+            </Label>
+            <Input id="email" className="col-span-3" defaultValue={email} />
+          </div>
+          {/* <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="password" className="text-left">
+              Password
+            </Label>
+            <Input id="password" className="col-span-3" />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="confirm_password" className="text-left">
+              Confirm password
+            </Label>
+            <Input id="confirm_password" className="col-span-3" />
+          </div> */}
+        </div>
+        <DialogFooter>
+          <div className="flex justify-end gap-2">
+            <Button variant="secondary" onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button type="submit" onClick={handleSubmit}>
+              Update admin
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default UpdateAdminModal;
