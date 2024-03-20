92cb2be95ef53bc274c64751240967684240e3cb
natnael772 <natnaeldeyas0@gmail.com>
2024-03-20 13:51:59 +0300

    feat: Dashboard page component

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PageHeader from "@/components/header/page-header";
+
+function Dashboard() {
+  return (
+    <div className="mx-auto w-full bg-muted rounded  mt-1 pb-4 ">
+      <PageHeader pageName="Dashboard" />
+      <div className="mx-auto w-[98%] h-fit bg-card rounded overflow-auto  mt-2   py-4 px-4  relative">
+        {/* <AdminTable admins={admins} /> */}
+        <h1>Hello, Natnael</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
