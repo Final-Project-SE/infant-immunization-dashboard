4cddf472d11b5105268ac92fee794a5e47edf190
natnael772 <natnaeldeyas0@gmail.com>
2024-03-20 14:15:14 +0300

    feat: Edit-Health-Station page for modifying health station data

---
+++ src/pages/edit-health-station.tsx
@@ -0,0 +1,18 @@
+import EditHealthStationForm from "@/components/form/edit-health-station-form";
+import PageHeader from "@/components/header/page-header";
+
+function EditHealthStationPage() {
+  return (
+    <>
+      <div className="mx-auto w-full rounded  mt-1 pb-4   h-[83vh]">
+        <PageHeader pageName="products / add" />
+
+        <div className="mx-auto w-[98%] light:bg-[#f9fafb]  dark:bg[#111827] rounded  mt-2 py-8 px-6  relative">
+          <EditHealthStationForm />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default EditHealthStationPage;
