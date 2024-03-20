8bfa8b36d63b4ce91731e254c6a4f6c9d578d272
natnael772 <natnaeldeyas0@gmail.com>
2024-03-20 13:55:01 +0300

    feat: Health stations page

---
+++ src/pages/health-stations.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import PageHeader from "@/components/header/page-header";
+
+function HealthStations() {
+  return (
+    <div className="mx-auto w-full bg-muted rounded  mt-1 pb-4 ">
+      <PageHeader pageName="HealthStations" />
+      <div className="mx-auto w-[98%] h-fit bg-card rounded overflow-auto  mt-2   py-4 px-4  relative">
+        <h1></h1>
+      </div>
+    </div>
+  );
+}
+
+export default HealthStations;
