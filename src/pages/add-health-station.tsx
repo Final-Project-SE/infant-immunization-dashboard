a4beece12b91b4bdee89084b17d63e391ebaa0ba
natnael772 <natnaeldeyas0@gmail.com>
2024-03-20 14:23:03 +0300

    feat: Add form for registering new station

--- src/pages/add-health-station.tsx
+++ src/pages/add-health-station.tsx
@@ -1,3 +1,4 @@
+import AddHealthStationForm from "@/components/form/add-health-station-form";
 import PageHeader from "@/components/header/page-header";
 
 function AddHealthStationPage() {
@@ -6,7 +7,9 @@ function AddHealthStationPage() {
       <div className="mx-auto w-full rounded  mt-1 pb-4   h-[83vh]">
         <PageHeader pageName="products / add" />
 
-        <div className="mx-auto w-[98%] light:bg-[#f9fafb]  dark:bg[#111827] rounded  mt-2 py-8 px-6  relative"></div>
+        <div className="mx-auto w-[98%] light:bg-[#f9fafb]  dark:bg[#111827] rounded  mt-2 py-8 px-6  relative">
+          <AddHealthStationForm onAdd={() => {}} />
+        </div>
       </div>
     </>
   );
