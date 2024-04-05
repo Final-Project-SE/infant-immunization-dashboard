67e86f4b47e245897d30219c60bf758723464e2b
atnabon <atnabond@gmail.com>
2024-04-05 20:44:22 +0300

    Create MothersPage component with MotherTable and dummy mother data

--- src/pages/MothersPage.tsx
+++ src/pages/MothersPage.tsx
@@ -1,63 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import PageHeader from '@/components/header/page-header';
 
-interface Mother {
-  motherId: number;
-  userId: number;
-  birthday: string;
-  bloodType: string;
-}
 
-const MothersPage: React.FC = () => {
-  const [mothers, setMothers] = useState<Mother[]>([]);
+import PageHeader from "@/components/header/page-header";
+import MotherTable from "@/components/table/mother-table";
+import { Mother } from "@/utils/types/component";
 
-  useEffect(() => {
-    const dummyData:Mother [] = [
-      {
-        motherId: 1,
-        userId: 101,
-        birthday: "1990-05-15",
-        bloodType: "A+",
-      },
-      {
-        motherId: 2,
-        userId: 102,
-        birthday: "1985-12-28",
-        bloodType: "B-",
-      },
-    ];
+// Dummy mother data
+const mothers: Mother[] = [
+  {
+    id: 1,
+    firstName: "Belatu",
+    lastName: "Jinka",
+    email: "belatu@example.com",
+    phone: "1234567890",
+    bloodType: "A+",
+    birthDate: new Date('1990-05-15'),
+    role: "mother",
+    otp: "123456",
+    activeStatus: "active",
+    createdDate: new Date('2024-04-05')
+  },
+  {
+    id: 2,
+    firstName: "gerawa",
+    lastName: "Jemberu",
+    email: "gerawa@example.com",
+    phone: "9876543210",
+    bloodType: "B+",
+    birthDate: new Date('1985-08-20'),
+    role: "mother",
+    otp: "654321",
+    activeStatus: "active",
+    createdDate: new Date('2024-04-06')
+  },
+  
+];
 
-    
-    setMothers(dummyData);
-  }, []);
-
+function MothersPage() {
   return (
     <div className="mx-auto w-full bg-muted rounded mt-1 pb-4">
       <PageHeader pageName="Mothers" />
       <div className="mx-auto w-[98%] h-fit bg-card rounded overflow-auto mt-2 py-4 px-4 relative">
-        <table className="w-full">
-          <thead>
-            <tr>
-              <th className="px-4 py-2">Mother ID</th>
-              <th className="px-4 py-2">User ID</th>
-              <th className="px-4 py-2">Birthday</th>
-              <th className="px-4 py-2">Blood Type</th>
-            </tr>
-          </thead>
-          <tbody>
-            {mothers.map((mother) => (
-              <tr key={mother.motherId}>
-                <td className="border px-4 py-2">{mother.motherId}</td>
-                <td className="border px-4 py-2">{mother.userId}</td>
-                <td className="border px-4 py-2">{mother.birthday}</td>
-                <td className="border px-4 py-2">{mother.bloodType}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <MotherTable mothers={mothers} />
       </div>
     </div>
   );
-};
+}
 
 export default MothersPage;
+
+
+
+
+
+
+
