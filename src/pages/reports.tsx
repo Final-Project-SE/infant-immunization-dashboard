a2b4bde269c47b9d576ae3069358a10544374476
natnael772 <natnaeldeyas0@gmail.com>
2024-04-20 14:30:40 +0300

    update: minor changes

--- src/pages/reports.tsx
+++ src/pages/reports.tsx
@@ -4,7 +4,7 @@ import PageHeader from "@/components/header/page-header";
 function Reports() {
   return (
     <div className="mx-auto w-full bg-muted rounded  mt-1 pb-4 ">
-      <PageHeader pageName="HealthProfessionals" />
+      <PageHeader pageName="Reports" />
       <div className="mx-auto w-[98%] h-fit bg-card rounded overflow-auto  mt-2   py-4 px-4  relative">
         <h1></h1>
       </div>
