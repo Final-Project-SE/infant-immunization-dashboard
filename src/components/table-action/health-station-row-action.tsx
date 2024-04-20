f61d9c2c426a6d4243f7de69f9bb4ae5b32d34d6
natnael772 <natnaeldeyas0@gmail.com>
2024-04-20 13:49:56 +0300

    feat: Row actions

---
+++ src/components/table-action/health-station-row-action.tsx
@@ -0,0 +1,73 @@
+import { Edit, Eye, ShieldX } from "lucide-react";
+import { MenuItem } from "@mui/material";
+import { Delete } from "@mui/icons-material";
+
+import UpdateAdminModal from "../modal/update-admin-modal";
+import ConfirmModal from "../modal/confirm-modal";
+import { useNavigate } from "react-router-dom";
+
+export const ViewDetailRowAction = ({ row }: { row: any }) => {
+  const navigate = useNavigate();
+  return (
+    <MenuItem
+      key="view"
+      onClick={() => navigate(`/healthstations/${row.id}`)}
+      className="text-gray-600 flex items-center gap-2"
+    >
+      <Eye className="text-green-500 text-sm" /> Detail
+    </MenuItem>
+  );
+};
+
+export const UpdateHealthStationRowAction = ({ row }: { row: any }) => {
+  const navigate = useNavigate();
+  return (
+    <UpdateAdminModal admin={row} onUpdate={() => {}}>
+      <MenuItem
+        key="edit"
+        onClick={() => navigate(`/health-stations/${row.id}/edit`)}
+        className="text-gray-600 flex items-center gap-2"
+      >
+        <Edit className="text-yellow-500 text-sm" /> Update
+      </MenuItem>
+    </UpdateAdminModal>
+  );
+};
+
+export const DeactivateHealthStationRowAction = ({ row }: { row: any }) => {
+  return (
+    // <ConfirmModal
+    //   actionName="Deactivate"
+    //   title="Deactivate Health Station"
+    //   description="Are you sure you want to deactivate this health station?"
+    //   onConfirm={() => {}}
+    // >
+    <MenuItem
+      key="deactivate"
+      onClick={() => {}}
+      className="text-gray-600 flex items-center gap-2"
+    >
+      <ShieldX className="text-red-500 text-sm" /> Deactivate
+    </MenuItem>
+    // </ConfirmModal>
+  );
+};
+
+export const DeleteHealthStationRowAction = ({ row }: { row: any }) => {
+  return (
+    <ConfirmModal
+      actionName="Delete"
+      title="Delete health station"
+      description="Are you sure you want to delete this data permanently? This action cannot be undone"
+      onConfirm={() => {}}
+    >
+      <MenuItem
+        key="delete"
+        onClick={() => console.info("Delete")}
+        className="text-gray-600 flex items-center gap-2"
+      >
+        <Delete className="text-red-500 text-sm" /> Delete
+      </MenuItem>
+    </ConfirmModal>
+  );
+};
