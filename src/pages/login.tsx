9257c9fbb6aef05fe99bc575e9cd0194f3b862a9
natnael772 <natnaeldeyas0@gmail.com>
2024-04-20 15:56:45 +0300

    style: minor styling updates

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -5,8 +5,8 @@ import LoginForm from "@/components/form/login-form";
 function Login() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-200 bg-[#f9fafb">
-      <Logo />;
-      <h4 className="text-2xl font-semibold text-center text-[#374151]">
+      <Logo />
+      <h4 className="text-2xl mt-2 font-semibold text-center text-[#374151]">
         Login to your account
       </h4>
       <LoginForm />
