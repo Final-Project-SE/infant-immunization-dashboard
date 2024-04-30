020a460bcfeb02d780663e8af20c0a0f93e4be58
atnabon <atnabond@gmail.com>
2024-05-01 00:30:20 +0300

    Add MothersPage component with data fetching

---
+++ src/api/useMothers.ts
@@ -0,0 +1,12 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import config from "@/configs/config";
+
+
+export default function useMothers() {
+  return useQuery({
+    queryKey: ['mothersData'],
+    queryFn: () =>
+      axios.get(`${config.BASE_URL}/mothers/`).then((res) => res.data),
+  });
+}
--- src/pages/mother.tsx
+++ src/pages/mother.tsx
@@ -1,54 +1,24 @@
+
 import PageHeader from "@/components/header/page-header";
 import MotherTable from "@/components/table/mother-table";
-
+import useMothers from "@/api/useMothers";
 import { Mother } from "@/utils/types/component";
+import { Spinner } from "@/components/ui";
+import Empty from "@/components/error-display/empty";
 
-// Dummy mother data
-const mothers: Mother[] = [
-  {
-    id: "1",
-    firstName: "Abebech",
-    lastName: "Kebede",
-    middleName: "Ayele",
-    phone: "0901020301",
-    birthDate: new Date(Date.now()),
-    bloodType: "A+",
-  },
-  {
-    id: "2",
-    firstName: "Abebech",
-    lastName: "Kebede",
-    middleName: "Ayele",
-    phone: "0911020304",
-    birthDate: new Date(Date.now()),
-    bloodType: "A+",
-  },
-  {
-    id: "3",
-    firstName: "Abebech",
-    lastName: "Kebede",
-    middleName: "Ayele",
-    phone: "0921020304",
-    birthDate: new Date(Date.now()),
-    bloodType: "A+",
-  },
-  {
-    id: "4",
-    firstName: "Abebech",
-    lastName: "Kebede",
-    middleName: "Ayele",
-    phone: "093102030",
-    birthDate: new Date(Date.now()),
-    bloodType: "A+",
-  },
-];
 
 function MothersPage() {
+  const { isFetching, error, data: mothers } = useMothers();
+
+  if (isFetching) return <Spinner />;
+
+  if (error) return <Empty resourceName="mothers" />;
+
   return (
     <div className="mx-auto w-full bg-muted rounded  mt-1 pb-4 ">
       <PageHeader pageName="Mothers" />
       <div className="mx-auto w-[98%] h-fit bg-card rounded overflow-auto  mt-2   py-4 px-4  relative">
-        <MotherTable mothers={mothers as any} />
+        <MotherTable mothers={mothers as Mother[]} />
       </div>
     </div>
   );
