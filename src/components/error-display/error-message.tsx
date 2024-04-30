80831d036cd376c26f035136a3d38d953ecdfc4d
natnael772 <natnaeldeyas0@gmail.com>
2024-04-30 23:35:40 +0300

    feat: Error display component

---
+++ src/components/error-display/error-message.tsx
@@ -0,0 +1,6 @@
+function ErrorMessage(error: any) {
+  console.log(error?.error?.message);
+  return <p className="text-red-500 px-6 py-3"> {error?.error?.message}</p>;
+}
+
+export default ErrorMessage;
