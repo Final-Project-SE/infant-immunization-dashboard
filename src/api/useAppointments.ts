6f650a4c0e69820f571a0792d2a154572fdfa380
atnabon <atnabond@gmail.com>
2024-05-01 00:37:08 +0300

    Add AppointmentsPage component with data fetching

---
+++ src/api/useAppointments.ts
@@ -0,0 +1,11 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import config from "@/configs/config";
+
+export default function useAppointments() {
+  return useQuery({
+    queryKey: ['appointmentsData'],
+    queryFn: () =>
+      axios.get(`${config.BASE_URL}//appointments`).then((res) => res.data),
+  });
+}
