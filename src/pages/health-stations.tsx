627fe02be6de2ffa719dbdd74a6406101c337d30
natnael772 <natnaeldeyas0@gmail.com>
2024-04-30 23:39:18 +0300

    feat: fetch and display hs

--- src/pages/health-stations.tsx
+++ src/pages/health-stations.tsx
@@ -1,30 +1,29 @@
 import Empty from "@/components/error-display/empty";
+
+import ErrorMessage from "@/components/error-display/error-message";
 import PageHeader from "@/components/header/page-header";
 import HealthStationTable from "@/components/table/health-station-table";
 import { Spinner } from "@/components/ui";
-import config from "@/configs/config";
+
+import { useGetHealthStations } from "@/hooks/api/health-station";
 import { HealthStation } from "@/utils/types/component";
-import { useQuery } from "@tanstack/react-query";
 
 function HealthStations() {
-  const {
-    isPending,
-    error,
-    data: healthstation,
-  } = useQuery({
-    queryKey: ["healthData"],
-    queryFn: () => fetch(config.BASE_URL).then((res) => res.json()),
-  });
+  const { isPending, error, hs } = useGetHealthStations();
+
+  console.log(hs);
 
   if (isPending) return <Spinner />;
 
-  if (error) return <Empty resourceName="health station" />;
+  if (error) return <ErrorMessage error={error} />;
+
+  if (!hs) return <Empty resourceName="health station" />;
 
   return (
     <div className="mx-auto w-full bg-muted rounded  mt-1 pb-4 ">
       <PageHeader pageName="health-stations" />
       <div className="mx-auto w-[98%] h-fit bg-card rounded overflow-auto  mt-2   py-4 px-4  relative">
-        <HealthStationTable healthStations={healthstation as HealthStation[]} />
+        <HealthStationTable healthStations={hs as HealthStation[]} />
       </div>
     </div>
   );
