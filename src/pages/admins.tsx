2cba72b1bfc652b2613b582d882e4c3487a7bbaa
atnabon <atnabond@gmail.com>
2024-05-02 22:40:53 +0300

    Add HealthProfessionals and Admin component

--- src/pages/admins.tsx
+++ src/pages/admins.tsx
@@ -1,72 +1,24 @@
-//  Components
+
+import React from 'react';
 import PageHeader from "@/components/header/page-header";
 import AdminTable from "@/components/table/admin-table";
+import { Admin } from "@/utils/types/component";
+import { useAdmins } from "@/api/useAdmins";
 import { Spinner } from "@/components/ui";
 import Empty from "@/components/error-display/empty";
 
-//  Types
-import { Admin } from "@/utils/types/component";
+const Admins: React.FC = () => {
+  const { isFetching, error, data: admins } = useAdmins();
 
-//  Hooks
-import { useQuery } from "@tanstack/react-query";
+  if (isFetching) return <Spinner />;
 
-// // Dummy admin data
-// const admins: Admin[] = [
-//   {
-//     id: 1,
-//     firstName: "Natnael",
-//     lastName: "Deyas",
-//     email: "natnaeldeyas@gmail.com",
-//     role: "super-admin",
-//     activeStatus: "active",
-//   },
-//   {
-//     id: 2,
-//     firstName: "Natnael",
-//     lastName: "Assefa",
-//     email: "nassefa@gmail.com",
-//     role: "admin",
-//     activeStatus: "active",
-//   },
-//   {
-//     id: 3,
-//     firstName: "Natnael",
-//     lastName: "Fekadu",
-//     email: "nafekadu@gmail.com",
-//     role: "admin",
-//     activeStatus: "active",
-//   },
-//   {
-//     id: 4,
-//     firstName: "Natnael",
-//     lastName: "Getachew",
-//     email: "natnaeln4d@gmail.com",
-//     role: "admin",
-//     activeStatus: "active",
-//   },
-// ];
+if (error) return <Empty resourceName="admins" />;
 
-function Admins() {
-  const {
-    isPending,
-    // error,
-    data: admins,
-  } = useQuery({
-    queryKey: ["adminsData"],
-    queryFn: () =>
-      fetch("https://jsonplaceholder.typicode.com/todos").then((res) =>
-        res.json()
-      ),
-  });
-
-  if (isPending) return <Spinner />;
-
-  if (!admins) return <Empty resourceName="admin" />;
 
   return (
-    <div className="mx-auto w-full bg-muted rounded  mt-1 pb-4 ">
+    <div className="mx-auto w-full bg-muted rounded mt-1 pb-4 ">
       <PageHeader pageName="Admins" />
-      <div className="mx-auto w-[98%] h-fit bg-card rounded overflow-auto  mt-2   py-4 px-4  relative">
+      <div className="mx-auto w-[98%] h-fit bg-card rounded overflow-auto mt-2 py-4 px-4 relative">
         <AdminTable admins={admins as Admin[]} />
       </div>
     </div>
--- src/pages/health-professionals.tsx
+++ src/pages/health-professionals.tsx
@@ -1,64 +1,26 @@
+
+import { HealthProfessionalProp } from "@/utils/types/component";
 import PageHeader from "@/components/header/page-header";
 import HealthProfessionalTable from "@/components/table/health-professional-table";
-import { HealthProfessionalProp } from "@/utils/types/component";
+import useHealthProfessionals from "@/api/useHealthProfessionals";
+import { Spinner } from "@/components/ui";
+import Empty from "@/components/error-display/empty";
 
-const healthProfessionals: HealthProfessionalProp[] = [
-  {
-    id: 1,
-    firstName: "John",
-    lastName: "Doe",
-    gender: "male",
-    imageUrl: "",
-    email: "johndoe@gmail.com",
-    phone: "0912345678",
-    role: "healthprofessional",
-    title: "Medical Doctor",
-    position: "Medical Doctor",
-  },
-  {
-    id: 2,
-    firstName: "Jane",
-    lastName: "Doe",
-    gender: "female",
-    imageUrl: "",
-    email: "janedoe@gmail.com",
-    phone: "0972345678",
-    role: "healthprofessional",
-    title: "Medical Doctor",
-    position: "Medical Doctor",
-  },
-  {
-    id: 3,
-    firstName: "Jane",
-    lastName: "Doe",
-    gender: "female",
-    imageUrl: "",
-    email: "janedoe@gmail.com",
-    phone: "0972345678",
-    role: "healthprofessional",
-    title: "Medical Doctor",
-    position: "Nurse",
-  },
-  {
-    id: 4,
-    firstName: "John",
-    lastName: "Doe",
-    gender: "male",
-    imageUrl: "",
-    email: "janedoe@gmail.com",
-    phone: "0972345678",
-    role: "healthprofessional",
-    title: "Medical Doctor",
-    position: "Health Assistant",
-  },
-];
+
+
 
 function HealthProfessionals() {
+  const { isFetching, error, data: healthProfessionals } = useHealthProfessionals();
+
+  if (isFetching) return <Spinner />;
+
+if (error) return <Empty resourceName="health-professional" />;
+
   return (
-    <div className="mx-auto w-full bg-muted rounded  mt-1 pb-4 ">
-      <PageHeader pageName="HealthProfessionals" />
-      <div className="mx-auto w-[98%] h-fit bg-card rounded overflow-auto  mt-2   py-4 px-4  relative">
-        <HealthProfessionalTable healthProfessionals={healthProfessionals} />
+    <div className="mx-auto w-full bg-muted rounded mt-1 pb-4">
+      <PageHeader pageName="Health Professionals" />
+      <div className="mx-auto w-[98%] h-fit bg-card rounded overflow-auto mt-2 py-4 px-4 relative">
+        <HealthProfessionalTable healthProfessionals={healthProfessionals as HealthProfessionalProp[]} />
       </div>
     </div>
   );
