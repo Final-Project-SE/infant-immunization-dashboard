0b87331e3d87a44a6d3d731cbf0e9b9d52355c79
atnabon <atnabond@gmail.com>
2024-05-09 22:52:39 +0300

    refactor useNews

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -5,7 +5,7 @@
 
 export default {
   // BASE_URL: "http://localhost:7777/api",
-  BASE_URL: "https://final-year-project-backend.onrender.com/api/",
+  BASE_URL: "https://final-year-project-backend.onrender.com/api",
   AUTH_TOKEN:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6IlNVUEVSIiwiZmlyc3ROYW1lIjoiTmF0bmFlbCIsImlhdCI6MTcxNDk3NjQ1OCwiZXhwIjoxNzE3NTY4NDU4fQ.-1rB4Ci6hsFJ8aZVL8uVQMoQPoVMFsVC282AHmv-S8U",
   // AUTH_TOKEN:
