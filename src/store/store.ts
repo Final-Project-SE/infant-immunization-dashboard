892257ff879e65af168fc5518cb6070e63c58c9f
natnael772 <natnaeldeyas0@gmail.com>
2024-05-10 13:11:46 +0300

    feat: auth store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import sidebarSlice from "@/features/sidebarSlice";
+import authReducer from "@/features/authSlice";
 
 const store = configureStore({
   reducer: {
     sidebar: sidebarSlice,
+    auth: authReducer,
   },
 });
+
+export type RootState = ReturnType<typeof store.getState>;
 
 export default store;
