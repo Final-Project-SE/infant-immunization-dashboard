a8c2b0f4aa07bc400fad0d4e3648e25c46b454fd
atnabon <atnabond@gmail.com>
2024-05-10 22:20:33 +0300

    Updated News component to use hooks for API calls

---
+++ src/components/modal/create-news-modal.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+function CreateNewsModal({ open, onOpenChange, onCreate }: { open: boolean, onOpenChange: (isOpen: boolean) => void, onCreate: (data: { title: string, description: string, imageUrl: string }) => void }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+
+  const handleClose = () => {
+    onOpenChange(false);
+  };
+
+  const handleSubmit = () => {
+    onCreate({ title, description, imageUrl });
+    handleClose();
+  };
+
+  return (
+    <Dialog open={open} onOpenChange={onOpenChange}>
+      <DialogTrigger asChild>
+        <Button variant="outline">Post News</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[650px] ">
+        <DialogHeader>
+          <DialogTitle>Post News</DialogTitle>
+          <DialogDescription>
+            Post a new news item. Enter the required details and click Post to create the news.
+          </DialogDescription>
+        </DialogHeader>
+
+        <div className="grid gap-4 py-4">
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="title" className="text-left">
+              Title
+            </Label>
+            <Input id="title" className="col-span-3" value={title} onChange={(e) => setTitle(e.target.value)} />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="description" className="text-left">
+              Description
+            </Label>
+            <Input id="description" className="col-span-3" value={description} onChange={(e) => setDescription(e.target.value)} />
+          </div>
+          <div className="grid grid-cols-4 items-center gap-4">
+            <Label htmlFor="imageUrl" className="text-left">
+              Image URL
+            </Label>
+            <Input id="imageUrl" className="col-span-3" value={imageUrl} onChange={(e) => setImageUrl(e.target.value)} />
+          </div>
+        </div>
+        <DialogFooter>
+          <div className="flex justify-end gap-2">
+            <Button variant="secondary" onClick={handleClose}>
+              Cancel
+            </Button>
+            <Button type="submit" onClick={handleSubmit}>
+              Post
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+export default CreateNewsModal;
