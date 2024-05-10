14501ec16a0de7bb40f4a49f56e2f9c261dd9c3f
natnael772 <natnaeldeyas0@gmail.com>
2024-05-10 13:12:12 +0300

    feat: Provide state

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { ErrorBoundary } from "react-error-boundary";
 import ErrorFallback from "./components/error-display/error-fallback.tsx";
 
 import "./index.css";
+import { StateProvider } from "./store/provider.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -13,7 +14,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       FallbackComponent={ErrorFallback}
       onReset={() => window.location.replace("/")}
     >
-      <App />
+      <StateProvider>
+        <App />
+      </StateProvider>
     </ErrorBoundary>
   </React.StrictMode>
 );
