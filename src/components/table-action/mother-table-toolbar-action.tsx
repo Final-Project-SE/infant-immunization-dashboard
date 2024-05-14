5d2157901f57ced43492bf89098edd35313b33dd
natnael772 <natnaeldeyas0@gmail.com>
2024-05-14 11:55:00 +0300

    feat

--- src/components/table-action/mother-table-toolbar-action.tsx
+++ src/components/table-action/mother-table-toolbar-action.tsx
@@ -1,8 +1,13 @@
 import CreateAdminModal from "../modal/create-admin-modal";
+import RegisterMotherModal from "../modal/register-mother-modal";
 import { Button } from "../ui/button";
 
 const MotherTableToolbarAction = () => {
-  return <Button>Register new mother</Button>;
+  return (
+    <RegisterMotherModal>
+      <Button>Register new mother</Button>
+    </RegisterMotherModal>
+  );
 };
 
 export default MotherTableToolbarAction;
